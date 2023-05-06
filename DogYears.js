8c2f698ab815102bfc4ddea4cd36c74b2769c46c
Caffeine <45335777+Aya-Ai-2022@users.noreply.github.com>
2023-05-06 20:57:16 +0300

    Create DogYears.js

---
+++ DogYears.js
@@ -0,0 +1,17 @@
+
+const myAge = 34;
+
+
+let earlyYears =2;
+earlyYears *=10.5;
+
+let laterYears = myAge - 2;
+
+laterYears*=4;
+
+console.log(`Early years are : ${earlyYears}`);
+console.log(`Later Years are : ${laterYears}`);
+let myAgeInDogYears = laterYears+ earlyYears;
+
+const myName = 'Aya'.toLowerCase();
+console.log(`My name is ${myName}. I am ${myAge} years old in human years which is DOG AGE years old in ${myAgeInDogYears}.`);
