0c1311ae19246bf40e740175fd0b653bc7f3843e
Caffeine <45335777+Aya-Ai-2022@users.noreply.github.com>
2023-05-11 22:31:29 +0300

    Update array_secret_message.js

--- array_secret_message.js
+++ array_secret_message.js
@@ -103,3 +103,16 @@ do {
   console.log(cupsAdded);
 } while (cupsAdded < cupsOfSugarNeeded);
 
+
+const rapperArray = ["Lil' Kim", "Jay-Z", "Notorious B.I.G.", "Tupac"];
+
+for (let i = 0; i < rapperArray.length; i++) {
+  if (rapperArray[i] === "Notorious B.I.G.") {
+    console.log(rapperArray[i]);
+    break;
+  }
+}
+
+console.log("And if you don't know, now you know.");
+
+
