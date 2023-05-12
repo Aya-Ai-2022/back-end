0ff345fc157be4978d09214505afc1054b59b574
Caffeine <45335777+Aya-Ai-2022@users.noreply.github.com>
2023-05-12 16:53:03 +0300

    Update javascript2.js n

--- javascript2.js
+++ javascript2.js
@@ -307,3 +307,234 @@ let spaceship = {
 let capFave = spaceship.crew.captain['favorite foods'][0];
 
 let firstPassenger = spaceship.passengers[0];
+
+
+let spaceship = {
+  'Fuel Type' : 'Turbo Fuel',
+  homePlanet : 'Earth'
+};
+
+// Write your code below
+
+let greenEnergy = obj => {
+  obj['Fuel Type'] = 'avocado oil';
+}
+
+let remotelyDisable = obj => {
+  obj.disabled = true;
+}
+
+greenEnergy(spaceship);
+
+remotelyDisable(spaceship);
+
+console.log(spaceship);
+
+
+let spaceship = {
+    crew: {
+    captain: { 
+        name: 'Lily', 
+        degree: 'Computer Engineering', 
+        cheerTeam() { console.log('You got this!') } 
+        },
+    'chief officer': { 
+        name: 'Dan', 
+        degree: 'Aerospace Engineering', 
+        agree() { console.log('I agree, captain!') } 
+        },
+    medic: { 
+        name: 'Clementine', 
+        degree: 'Physics', 
+        announce() { console.log(`Jets on!`) } },
+    translator: {
+        name: 'Shauna', 
+        degree: 'Conservation Science', 
+        powerFuel() { console.log('The tank is full!') } 
+        }
+    }
+}; 
+
+
+
+for (let crewMember in spaceship.crew) {
+  console.log(`${crewMember}: ${spaceship.crew[crewMember].name}`)
+};
+
+for (let crewMember in spaceship.crew) {
+  console.log(`${spaceship.crew[crewMember].name}: ${spaceship.crew[crewMember].degree}`)
+};
+
+////////////////////////
+const robot = {
+  model: 'B-4MI',
+  mobile: true,
+  greeting() {
+  	console.log(`I'm model ${this.model}, how may I be of service?`);
+  }
+}
+
+const massProdRobot = (model, mobile) => {
+  return {
+    model,
+    mobile,
+    greeting() {
+      console.log(`I'm model ${this.model}, how may I be of service?`);
+    }
+  }
+}
+
+const shinyNewRobot = massProdRobot('TrayHax', true)
+
+const chargingStation = {
+  _name: 'Electrons-R-Us',
+  _robotCapacity: 120,
+  _active: true,
+  _chargingRooms: ['Low N Slow', 'Middle of the Road', 'In and Output'],
+
+  set robotCapacity(newCapacity) {
+    if (typeof newCapacity === 'number') {
+      this._robotCapacity = newCapacity;
+    } else {
+      console.log(`Change ${newCapacity} to a number.`)
+    }
+  },
+  get robotCapacity() {
+    return this._robotCapacity;
+  }
+}
+///////////////////
+
+const robot = {
+  model: "1E78V2",
+  energyLevel: 100,
+  provideInfo() {
+    return `I am ${this.model} and my current energy level is ${this.energyLevel}.`;
+  },
+};
+console.log(robot.provideInfo());
+///////////////////////
+const robot = {
+  energyLevel: 100,
+  checkEnergy(){
+    console.log(`Energy is currently at ${this.energyLevel}%.`)
+  }
+}
+
+robot.checkEnergy();
+
+
+const robot = {
+  _energyLevel: 100,
+  recharge(){
+    this._energyLevel += 30;
+    console.log(`Recharged! Energy is currently at ${this._energyLevel}%.`)
+  }
+};
+
+robot._energyLevel = 'high';
+robot.recharge();
+
+const robot = {
+  _model: '1E78V2',
+  _energyLevel: 100,
+  get energyLevel(){
+    if(typeof this._energyLevel === 'number') {
+      return 'My current energy level is ' + this._energyLevel
+    } else {
+      return "System malfunction: cannot retrieve energy level"
+    }
+  }
+};
+
+console.log(robot.energyLevel);
+
+const robot = {
+  _model: "1E78V2",
+  _energyLevel: 100,
+  _numOfSensors: 15,
+  set numOfSensors(num) {
+    if (typeof num === "number" && num >= 0) {
+      this._numOfSensors = num;
+    } else {
+      console.log("Pass in a number that is greater than or equal to 0");
+    }
+  },
+  get numOfSensors() {
+    if (typeof this._numOfSensors === "number") {
+      return this._numOfSensors;
+    } else {
+      return "Sensors are currently down.";
+    }
+  },
+};
+robot.numOfSensors = 100;
+console.log(robot.numOfSensors);
+const robotFactory = (model, mobile) => {
+  return {
+    model: model,
+    mobile: mobile,
+    beep() {
+      console.log("Beep Boop");
+    },
+  };
+};
+
+const tinCan = robotFactory("P-500", true);
+tinCan.beep();
+
+const robotFactory = (model, mobile) => {
+  return {
+    model,
+    mobile,
+    beep() {
+      console.log('Beep Boop');
+    }
+  }
+}
+
+
+const newRobot = robotFactory('P-501', false)
+console.log(newRobot.model)
+console.log(newRobot.mobile);
+
+
+const robot = {
+  model: '1E78V2',
+  energyLevel: 100,
+  functionality: {
+    beep() {
+      console.log('Beep Boop');
+    },
+    fireLaser() {
+      console.log('Pew Pew');
+    },
+  }
+};
+ const {functionality}= robot;
+ functionality.beep();
+
+const robot = {
+  model: "SAL-1000",
+  mobile: true,
+  sentient: false,
+  armor: "Steel-plated",
+  energyLevel: 75,
+};
+
+
+const robotKeys = Object.keys(robot);
+
+console.log(robotKeys);
+
+
+const robotEntries = Object.entries(robot);
+console.log(robotEntries);
+
+
+const newRobot = Object.assign(
+  { laserBlaster: true, voiceRecognition: true },
+  robot
+);
+
+console.log(newRobot);
