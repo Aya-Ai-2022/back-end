8ad01e298c2c585e1a689115003d4cb203201cff
Caffeine <45335777+Aya-Ai-2022@users.noreply.github.com>
2023-05-12 12:48:20 +0300

    Update javascript2.js

--- javascript2.js
+++ javascript2.js
@@ -215,3 +215,95 @@ for (const letter of pokemonList) {
 console.log(`You caught a ${letter}!`);
 }
 
+/////////////objects///////////////////////////////////////////////////////////////
+
+
+let fasterShip = {
+  "Fuel Type": "Turbo Fuel",
+  color: "silver",
+};
+
+let spaceship = {
+  homePlanet: "Earth",
+  color: "silver",
+  "Fuel Type": "Turbo Fuel",
+  numCrew: 5,
+  flightPath: ["Venus", "Mars", "Saturn"],
+};
+
+let crewCount = spaceship.numCrew;
+let planetArray = spaceship.flightPath;
+
+
+let spaceship = {
+  "Fuel Type": "Turbo Fuel",
+  "Active Mission": true,
+  homePlanet: "Earth",
+  numCrew: 5,
+};
+
+let propName = "Active Mission";
+
+let isActive = spaceship[propName];
+console.log(isActive);
+
+let spaceship = {
+  'Fuel Type' : 'Turbo Fuel',
+  homePlanet : 'Earth',
+  color: 'silver',
+  'Secret Mission' : 'Discover life outside of Earth.'
+};
+
+
+
+spaceship.color = 'glorious gold';
+
+spaceship.numEngines = 9;
+
+delete spaceship['Secret Mission'];
+
+let retreatMessage =
+  "We no longer wish to conquer your planet. It is full of dogs, which we do not care for.";
+const alienShip = {
+  retreat() {
+    console.log(retreatMessage);
+  },
+  takeOff() {
+    console.log("Spim... Borp... Glix... Blastoff!");
+  },
+};
+alienShip.retreat();
+alienShip.takeOff();
+
+let spaceship = {
+  passengers: [{name: 'Space Dog'}], 
+  telescope: {
+    yearBuilt: 2018,
+    model: "91031-XLT",
+    focalLength: 2032 
+  },
+  crew: {
+    captain: { 
+      name: 'Sandra', 
+      degree: 'Computer Engineering', 
+      encourageTeam() { console.log('We got this!') },
+     'favorite foods': ['cookies', 'cakes', 'candy', 'spinach'] }
+  },
+  engine: {
+    model: "Nimbus2000"
+  },
+  nanoelectronics: {
+    computer: {
+      terabytes: 100,
+      monitors: "HD"
+    },
+    'back-up': {
+      battery: "Lithium",
+      terabytes: 50
+    }
+  }
+}; 
+
+let capFave = spaceship.crew.captain['favorite foods'][0];
+
+let firstPassenger = spaceship.passengers[0];
