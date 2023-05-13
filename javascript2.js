b63c3223986363a69b31313f3f3047150e001ddd
Caffeine <45335777+Aya-Ai-2022@users.noreply.github.com>
2023-05-13 12:28:03 +0300

    Update javascript2.js add Itrators

--- javascript2.js
+++ javascript2.js
@@ -538,3 +538,150 @@ const newRobot = Object.assign(
 );
 
 console.log(newRobot);
+/////////////////////////////////////Itrators//////////////////////////
+
+
+const artists = ['Picasso', 'Kahlo', 'Matisse', 'Utamaro'];
+
+artists.forEach(artist => {
+  console.log(artist + ' is one of my favorite artists.');
+});
+
+const numbers = [1, 2, 3, 4, 5];
+
+const squareNumbers = numbers.map(number => {
+  return number * number;
+});
+
+console.log(squareNumbers);
+
+const things = ['desk', 'chair', 5, 'backpack', 3.14, 100];
+
+const onlyNumbers = things.filter(thing => {
+  return typeof thing === 'number';
+});
+
+console.log(onlyNumbers);
+
+const fruits = ['mango', 'papaya', 'pineapple', 'apple'];
+
+fruits.forEach(fruit => console.log(`I want to eat a ${fruit}.`));
+
+
+const animals = ['Hen', 'elephant', 'llama', 'leopard', 'ostrich', 'Whale', 'octopus', 'rabbit', 'lion', 'dog'];
+
+
+const secretMessage = animals.map(animal => animal[0]);
+
+console.log(secretMessage.join(''));
+
+const bigNumbers = [100, 200, 300, 400, 500];
+
+
+const smallNumbers = bigNumbers.map(num => num/100);
+
+console.log(smallNumbers);
+
+const randomNumbers = [375, 200, 3.14, 7, 13, 852];
+
+const smallNumbers = randomNumbers.filter((num) => {
+  return num < 250;
+});
+
+const favoriteWords = [
+  "nostalgia",
+  "hyperbole",
+  "fervent",
+  "esoteric",
+  "serene",
+];
+
+const longFavoriteWords = favoriteWords.filter((word) => {
+  return word.length > 7;
+});
+
+
+const animals = [
+  "hippo",
+  "tiger",
+  "lion",
+  "seal",
+  "cheetah",
+  "monkey",
+  "salamander",
+  "elephant",
+];
+
+const foundAnimal = animals.findIndex((animal) => {
+  return animal === "elephant";
+});
+console.log(foundAnimal);
+
+const startsWithS = animals.findIndex((animal) => {
+  return animal[0] === "s";
+});
+console.log(startsWithS);
+
+const newNumbers = [1, 3, 5, 7];
+
+const newSum = newNumbers.reduce((accumulator, currentValue) => {
+
+  console.log("The value of accumulator: ", accumulator);
+  console.log("The value of currentValue: ", currentValue);
+  return accumulator + currentValue;
+}, 10);
+console.log(newSum);
+
+const words = ["unique", "uncanny", "pique", "oxymoron", "guise"];
+
+console.log(
+  words.some((word) => {
+    return word.length < 6;
+  })
+);
+
+const interestingWords = words.filter((word) => {
+  return word.length > 5;
+});
+console.log(interestingWords);
+
+console.log(
+  interestingWords.every((word) => {
+    return word.length > 5;
+  })
+);
+const cities = [
+  "Orlando",
+  "Dubai",
+  "Edinburgh",
+  "Chennai",
+  "Accra",
+  "Denver",
+  "Eskisehir",
+  "Medellin",
+  "Yokohama",
+];
+
+const nums = [1, 50, 75, 200, 350, 525, 1000];
+
+
+cities.forEach((city) => console.log("Have you visited " + city + "?"));
+
+
+const longCities = cities.filter((city) => city.length > 7);
+
+
+const word = cities.reduce((acc, currVal) => {
+  return acc + currVal[1];
+}, "c");
+
+console.log(word);
+
+
+const smallerNums = nums.map((num) => num - 5);
+
+
+nums.every((num) => num < 0);
+// OR nums.some(num => num < 0);
+
+
