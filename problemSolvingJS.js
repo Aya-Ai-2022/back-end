84cab84bdabd365a290df00c7137b3c570c22e8a
Caffeine <45335777+Aya-Ai-2022@users.noreply.github.com>
2023-05-19 18:35:24 +0300

    Update problemSolvingJS.js 15_problems

--- problemSolvingJS.js
+++ problemSolvingJS.js
@@ -388,3 +388,104 @@ const groceries = list => {
   
   return listString;
 }
+
+///////class/////
+/*
+You are a college student and you want to stream a movie over one of the campus’s Wi-Fi networks. Based on the number of users, some networks won’t have the data to stream a movie.
+
+Use the following Network class to track how much total data and how many users each network has:
+The properties of the Network class are:
+
+data: Total units of data supplied by the network
+users: Total numbers of users currently on the network
+Each user on average deducts 5 units from the network’s total data. To watch a movie you must connect to a network that has at least 10 remaining units of data.
+
+Add a method movieTime() to the Network class that returns true if there is enough data available to watch a movie, false if there isn’t.
+
+Example:
+
+const library = new Network(50, 9) 
+library.movieTime() // returns false
+*/
+class Network {
+  constructor(data, users) {
+    this.data = data;
+    this.users = users;
+  }
+  
+  movieTime(){
+    return this.data/this.users >=10?true:false;
+  }
+  
+}
+const library = new Network(50, 9) ;
+console.log(library.movieTime());
+
+
+
+
+/////////////
+
+/*
+A video game consists of two players floating using 100 helium balloons. The players shoot pellets at each other’s balloons and after 10 minutes the player with the most balloons left wins.
+
+class Player {
+  constructor(name, hitsPerMinute) {
+    this.name = name;
+    this.hitsPerMinute = hitsPerMinute;
+    this.balloonCount = 100;
+  }
+ 
+  status() {
+    console.log(`Player: ${this.name} -- Balloons Left: ${this.balloonCount}`)
+  }
+}
+Write a game function balloonAttack that takes two Player instances, calculates the balloons left for each player after 10 minutes (using the hitsPerMinute property) and returns the name of the winner. If the result is a tie, return the string 'Tie'.
+
+You can test your balloonAttack function by creating two instances of the Player class to use as arguments for your function like below:
+
+const p1 = new Player('p1', 5);
+const p2 = new Player('p2', 2);
+ 
+balloonAttack(p1, p2);
+
+
+*/
+
+class Player {
+  constructor(name, hitsPerMinute) {
+    this.name = name;
+    this.hitsPerMinute = hitsPerMinute;
+    this.balloonCount = 100;
+  }
+
+  status() {
+    console.log(`Player: ${this.name} -- Balloons Left: ${this.balloonCount}`)
+  }
+}
+
+// Write function below
+const balloonAttack = (player1, player2) => {
+  for (let i = 1; i <= 10; i++){
+    player2.balloonCount -= player1.hitsPerMinute
+    player1.balloonCount -= player2.hitsPerMinute
+    player1.status();
+    player2.status();
+  }
+  if (player1.balloonCount > player2.balloonCount) {
+    return player1.name;
+  }else if (player2.balloonCount > player1.balloonCount)
+    return player2.name
+
+  return 'Tie'
+}
+
+
+
+
+
+
+
+
+
+
